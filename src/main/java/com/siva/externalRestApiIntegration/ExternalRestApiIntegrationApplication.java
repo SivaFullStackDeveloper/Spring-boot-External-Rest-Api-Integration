955d0747649e974d7d38bec17fbca462cdef3f46
SivaFullStackDeveloper <sivaflutterdeveloper@gmail.com>
2024-11-23 17:23:50 +0530

    initial commit

---
+++ src/main/java/com/siva/externalRestApiIntegration/ExternalRestApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.siva.externalRestApiIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalRestApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalRestApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siva/externalRestApiIntegration/ExternalRestApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.externalRestApiIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalRestApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
